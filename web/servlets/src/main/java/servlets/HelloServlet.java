9d3519ce191ce49611c61e8d86dda010689db664
Gustavo Silva <Gustavo_Silva@McKinsey.com>
2024-11-12 16:57:12 -0300

    configure servlet as a maven dependency instead of add as a jar in the project

--- web/servlets/src/main/java/servlets/HelloServlet.java
+++ web/servlets/src/main/java/servlets/HelloServlet.java
@@ -1,13 +1,11 @@
 package servlets;
 
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.awt.*;
 import java.io.IOException;
 
 @WebServlet(name = "HelloServlet", urlPatterns = "/hello")
@@ -17,7 +15,8 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         resp.getWriter().print("");
-        resp.getWriter().print("Here is another line");    }
+        resp.getWriter().print("Here is another line");
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
